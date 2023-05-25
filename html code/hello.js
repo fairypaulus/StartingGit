92cacea5f79476a5d8ab6466aaecb10344abbca4
fairypaulus <fairylal14@gmail.com>
2023-05-26 01:20:52 +0530

    my ocde js

---
+++ html code/hello.js
@@ -0,0 +1,13 @@
+// console.dir(document);
+// console.log(document.title);
+// // document.title="hello fairy";
+// console.log(document.domain);
+// console.log(document.URL);
+
+
+let header=document.getElementById("main-header");
+header.style.borderBottom="solid 4px #000";
+
+document.getElementById("additem").style.fontWeight="bold";
+document.getElementById("additem").style.backgroundColor="green";
+
